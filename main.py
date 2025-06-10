76374b42b11f5948fa6574ab0dcac9afdecfea4d
jacksonsolomondavid <davidsolomoncheruvathur@gmail.com>
2025-06-10 16:02:13 +0530

    added frontend using fastapi

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5a549d46016830172b3f41b8e17a061afd9686f09546bfd1193c3b454e87cb16
-size 921
+oid sha256:1ee14e12748c8eb0c87f6c918e3a8f0b25b6a0e79175dacf5bbdb3fa912f2d15
+size 4980
