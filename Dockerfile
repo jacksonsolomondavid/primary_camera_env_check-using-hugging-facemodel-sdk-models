900bcd9b47f1bd0a590a9dd5ac68d07d8d9bcda8
jacksonsolomondavid <davidsolomoncheruvathur@gmail.com>
2025-06-19 15:27:20 +0530

    docker part is added

---
+++ Dockerfile
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:549e7ac64cdc68e3a221dcb7f8754d813bc12a1f87bf0c9280f1fb94c3295891
+size 1243
--- detectors/object_detector.py
+++ detectors/object_detector.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ede11b90df8324d0862939c7ca0dda47b07177070734ef2ad0ffa230d9ee9895
-size 2885
+oid sha256:4d6fca0e31161c0171167f780fd3877193d941cc2d7f6c9c57240e2f14fad8f0
+size 2886
